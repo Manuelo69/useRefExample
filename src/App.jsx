7dd1a0a7f7ce315074c2365091bea5ddde94a317
manuelo69 <ichigo2418@gmail.com>
2024-04-23 12:21:43 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Contador from "./components/Contador";
+import MensajePrevio from "./components/MensajePrevio";
+import PulsaBotones from "./components/PulsaBotones";
+
+function App() {
+  return (
+    <>
+      <PulsaBotones></PulsaBotones>
+      <MensajePrevio></MensajePrevio>
+    </>
+  );
+}
+
+export default App;
